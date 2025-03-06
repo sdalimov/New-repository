20a922ceeb4e067a901aae59cbbaacecbb213214
sdalimov <sherozbekdolimov@gmail.com>
2025-03-06 17:43:15 +0500

    Исправлены конфликты, финальное обновление

--- leaderboard_server.js
+++ leaderboard_server.js
@@ -55,7 +55,7 @@ app.post("/submit-score", (req, res) => {
     res.json({ message: "Результат сохранен" });
 });
 
-// Запуск сервера на 0.0.0.0 для работы на Render
+// Запуск сервера на 0.0.0.0 для Render
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
